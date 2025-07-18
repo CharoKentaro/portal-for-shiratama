926dc74f51201770691c559e30bc900a1f10310e
Charo <122759581+CharoKentaro@users.noreply.github.com>
2025-07-18 20:09:28 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="シラタマ・カスタム", page_icon="⚔️")
+
+st.title("⚔️ シラタマさん専用AIアシスタント")
+st.header("現在、最終調整中です。")
+st.info("このアプリの、住所（URL）を、取得するために、一時的に、この画面を、表示しています。")
+st.success("このページの、URLを、コピーして、AIアシスタントに、教えてください！")
